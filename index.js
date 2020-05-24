d4560ca2c9b0ef001ff82114989c93314f62bf3d
Raiishin <60734119+Raiishin@users.noreply.github.com>
2020-05-24 17:25:39 +0800

    Initial Commit
    Added in export for isPalindrome function

---
+++ index.js
@@ -0,0 +1,7 @@
+export const isPalindrome = (string) => {
+  const str = string.toLowerCase().replace(/[\W_]/g, "");
+  for (let i=0, len = str.length-1;i<len/2;i++) {
+    if (str[i] !== str[len-i]) return false
+  }
+  return true
+}
